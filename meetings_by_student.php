e99c454eabc5a6d965a08b13b72fb5ab7ba9ef29
shanqincheng <codingstory@outlook.com>
2022-05-11 00:14:02 +1000

    rollback link item to original style

--- meetings_by_student.php
+++ meetings_by_student.php
@@ -12,7 +12,7 @@ $row1=$getStudentDetails->fetch(PDO::FETCH_ASSOC);
 ?>
 
 <div class="visual-space"></div>
-<button type="submit" class="black-button"><a class="white-link" href="search-student-privacy.php?search=students&searchStudentID=<?php echo $_GET['searchStudentID']; ?>&searchStudentGN=<?php echo $_GET['searchStudentGN']; ?>&searchStudentFN=<?php echo $_GET['searchStudentFN']; ?>">Go back</a></button>
+<a href="search-student-privacy.php?search=students&searchStudentID=<?php echo $_GET['searchStudentID']; ?>&searchStudentGN=<?php echo $_GET['searchStudentGN']; ?>&searchStudentFN=<?php echo $_GET['searchStudentFN']; ?>">Go back</a>
 <h2 class="page-label">View All Meetings for Student:</h2>
 <h4 class="page-label">ID: <?php echo $_GET['id']; ?></h4>
 <h4 class="page-label">Name: <?php echo $row1['given_name']." ".$row1['family_name']; ?></h4>
